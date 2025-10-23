4440a123ceb81c90818cdce38f790285ba03cc00
Vasilev-Stefan <sbasilev@gmail.com>
2025-10-24 01:03:32 +0300

    Add login action

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -8,3 +8,15 @@ userController.post('/register', async (req, res) => {
     const user = await userService.register(userData);
     res.status(201).json(user);
 });
+
+userController.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+    
+    try {
+        const user = await userService.login(email, password);
+        res.json(user)
+    } catch (error) {
+        console.log(error)
+        res.status(401).json({message: error.message})
+    }
+})
--- api/src/service/userService.js
+++ api/src/service/userService.js
@@ -1,4 +1,5 @@
 import User from "../models/user.js";
+import bcrypt from 'bcrypt';
 
 
 
@@ -9,6 +10,24 @@ async function register(userData) {
     return user;
 }
 
+async function login(email, password) {
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw new Error('Invalid email or password!');
+    }
+
+    const isValid = bcrypt.compare(password, user.password);
+
+    if(!isValid){
+        throw new Error('Invalid email or password!');
+    }
+
+    console.log('User sucessfully logged!');
+    return user;
+}
+
 export const userService = {
-    register
+    register,
+    login
 }
