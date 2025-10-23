8076361f36e77b257184cfdc458f51c8e004d57a
Vasilev-Stefan <sbasilev@gmail.com>
2025-10-23 23:59:29 +0300

    Add cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 
 app.listen(3030, () => {
     console.log('Server is listening on http://localhost:3030...')
